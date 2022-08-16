c392bb7f374dcd5797b44cacdc3df6ced5a33199
Alexander Filippov <sash.f@rambler.ru>
2022-08-16 13:04:22 +0300

    work4_V2

--- Script_select.sql
+++ Script_select.sql
@@ -22,11 +22,10 @@ group by a.title;
 
 №4 все исполнители, которые не выпустили альбомы в 2020 году
 
-select p.name, year_release  from albums a 
-join performers_albums pa on a.id = pa.id_albums
-join performers p on pa.id_the_performer = p.id   
-where year_release != 2020
-group by p.name, year_release; 
+select p.name FROM performers p 
+where p.id not in (select distinct pa.id_The_performer from albums a  
+join performers_albums pa on a.id = pa.id_albums 
+where a.year_release = 2020); 
 
 
 #5 названия сборников, в которых присутствует конкретный исполнитель (выберите сами)
@@ -37,8 +36,7 @@ join albums a on pa.id_albums  = a.id
 join tracks t on a.id  = t.id_albums
 join tracks_collection tc on t.id = tc.id_track  
 join collection c on c.id = tc.id_collection 
-where p.name  like 'Мумий троль'
-group by p.name, t.title, c.title;
+where p.name  like 'Мумий троль';
 
 
 #6 название альбомов, в которых присутствуют исполнители более 1 жанра
@@ -56,8 +54,7 @@ having count(pmg.id_music_genre) > 1;
 select t.title, t.id  from collection c 
 join tracks_collection tc on c.id = tc.id_collection  
 full join tracks t on t.id = tc.id_track
-where c.id is NULL
-group by t.title, t.id;
+where c.id is NULL;
 
 
 №8 исполнителя(-ей), написавшего самый короткий по продолжительности трек
@@ -74,15 +71,16 @@ select p.name, t.title, t.duration  from performers p
 join performers_albums pa on p.id = pa.id_the_performer 
 join albums a on pa.id_albums  = a.id 
 join tracks t on a.id  = t.id_albums
-where t.duration = (select min(duration) from tracks t2)
-group by p.name, t.title, t.duration;
+where t.duration = (select min(duration) from tracks t2);
 
 
 #9 название альбомов, содержащих наименьшее количество треков
 
-select a.title, count(t.id_albums)  from albums a 
-join tracks t on a.id  = t.id_albums
-group by a.title
-order by count(t.id_albums) 
-limit 3;
+select a1.title title, count(t1.id) Col_t from albums a1 
+join tracks t1 on a1.id  = t1.id_albums  
+group by a1.title  
+having (count(t1.id) = (select min(t2.Col_t)  
+from (select a.title title, count(t.id_albums) Col_t  
+from albums a join tracks t on a.id  = t.id_albums  
+group by a.title) as t2));
 
